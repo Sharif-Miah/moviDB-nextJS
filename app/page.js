ada5241c4095f23730fd78e6e2ebc39309b0e259
Sharif-Miah <farhansharif1410@gmail.com>
2025-02-28 12:03:33 +0600

    trending and popular movie section added

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
 import HeroSection from '@/components/Hero';
+import PopularMovies from '@/components/PopularMovies';
+import TrendingMovies from '@/components/TrendingMovies';
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
+      <div className='container mx-auto px-4 py-8'>
+        <TrendingMovies />
+        <PopularMovies />
+      </div>
     </div>
   );
 }
